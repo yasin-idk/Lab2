25fc75068ab7a52a26f81b04e58e53b28cff03e0
Yasin ÇEŞMECİ <yasincesmeci@eskisehir.edu.tr>
2022-10-31 16:21:53 +0300

    app javada extra hell

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.println( "This world can go to hell!" );
+		System.out.println( "This world can go to hell!" );
+		System.out.println( "This world can go to hell!" );
     }
 }
